3e0236764f74a7e478371c1111e5f5bf95e60abd
JDHinds <jude.hinds@gmail.com>
2025-05-06 18:17:09 +0100

    Create Flutter Project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tinydungeon
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tinydungeon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tinydungeon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
